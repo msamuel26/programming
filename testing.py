271fa12698b3e8ea671d8bade57e3c75f56fec79
Moses Samuel <msamuelmulia@gmail.com>
2021-09-23 20:16:42 +0700

    malam ini, mempelajari nilai dalam format input (manual) dan di debug

--- testing.py
+++ testing.py
@@ -1,10 +1,16 @@
 def nilai_ganjil(value):
-    if value % 2 == 1:
-        return True
-    else:
-        return False
+    hasil_cek = None
 
-if nilai_ganjil(101):
+    if value % 2 == 1:
+        hasil_cek = True
+    else:
+        hasil_cek = False
+
+    return hasil_cek
+
+nilai = int(input('Nilai berapa yang akan dicek? : '))
+
+if nilai_ganjil(nilai):
     print("nilai tersebut adalah ganjil")
 else:
     print("nilai tersebut adalah genap")
