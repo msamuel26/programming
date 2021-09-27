77b2cd956f5d886a2bc222fab2e1e7ec2537feb9
Moses Samuel <msamuelmulia@gmail.com>
2021-09-27 20:34:24 +0700

    hari ini mempelajari: identation dan terserah menamakan suatu variabel

--- prime2.py
+++ prime2.py
@@ -1,14 +1,14 @@
-def check_prime_number(value):
-    flag = False
+def ingin_mengecek_bilangan_prima(value):
+    marker = True
     if value > 1:
-        for i in range(2, value):
-            if value % 1 == 0:
-                flag = True
+        for m in range(2, value):
+            if value % m == 0:
+                marker = False
                 break
-    return not flag
-
-test = int(input("Please select any number: "))
-if check_prime_number(test):
-    print(test, "is prime number")
+    return marker
+    
+test = int(input("Please choose your number: "))
+if ingin_mengecek_bilangan_prima(test):
+    print(test, "adalah bilangan prima")
 else:
-    print(test, "isn't prime number") 
+    print(test, "bukan bilangan prima")
