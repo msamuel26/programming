5dbc37d49f52f012ecc80cb2bb312db99bab5833
Moses Samuel <msamuelmulia@gmail.com>
2021-09-28 19:59:13 +0700

    hari ini mempelajari comment untuk memberikan penjelasan atas kode yang diberikan

--- leapyear.py
+++ leapyear.py
@@ -1,5 +1,17 @@
+# The Gregorian leap year rule is:
+#   1. If the year is evenly divisible by 4, go to step 2. Otherwise, go to step 5.
+#   2. If the year is evenly divisible by 100, go to step 3. Otherwise, go to step 4.
+#   3. If the year is evenly divisible by 400, go to step 4. Otherwise, go to step 5.
+#   4. The year is a leap year (it has 366 days).
+#   5. The year is not a leap year (it has 365 days).
+#   from https://docs.microsoft.com/en-us/office/troubleshoot/excel/determine-a-leap-year
+# 
+#   Check your function with this leap calculator
+#   https://www.omnicalculator.com/everyday-life/leap-year#what-is-a-leap-year
+
 def check_leap_year(year):
     leap = False
+    # https://www.history.com/news/6-things-you-may-not-know-about-the-gregorian-calendar
     if year > 1582:
         if year % 4 == 0:
             leap = True
