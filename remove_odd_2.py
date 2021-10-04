3c5264b57b7108c5a1a293beb02a33b0b61885ba
Moses Samuel <msamuelmulia@gmail.com>
2021-10-04 21:36:53 +0700

    hari ini mempelajari cara me-remove bilangan ganjil dan bilangan genap dalam suatu list dengan menggunakan negasi

--- remove_odd_2.py
+++ remove_odd_2.py
@@ -12,6 +12,18 @@ def remove_odd(list):
             list.remove(value)
     return list
 
-p = [1, 2, 7, 8, 4, 3, 6, 8, 4, 6, 7, 5]
-assert remove_odd(p) == [2, 4, 4, 6, 6, 8, 8]
-print(p)
+def remove_even(list):
+    list.sort()
+    dumy = list.copy()
+    for value in dumy:
+        if not is_odd(value):
+            list.remove(value)
+    return list
+
+x = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+assert remove_odd(x) == [2, 2, 4, 4]
+print(x)
+
+a = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5]
+assert remove_even(a) == [1, 1, 3, 3, 5, 5]
+print(a)
