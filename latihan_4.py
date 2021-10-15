d8111a07fea5e3e433b3af0b393785a9eb408f4f
Moses Samuel <msamuelmulia@gmail.com>
2021-10-15 14:03:33 +0700

    Hari ini mempertajam pemahaman tentang dictionary

--- latihan_4.py
+++ latihan_4.py
@@ -73,10 +73,33 @@ def shown_only_even_key_and_with_more_than_1_in_value(input_dict):
             output_dict[key] = input_dict[key]
     return output_dict
 
+def shown_only_odd_key_and_with_more_than_1_in_value(input_dict):
+    output_dict = {}
+    for key in input_dict:
+        if odd_number[key] and input_dict[key] > 1:
+            output_dict[key] = input_dict[key]
+    return output_dict
+
 unyil = create_summary(numbers)
 # print(unyil)
-# print(shown_only_even_key(unyil))
-# print(shown_only_odd_key(unyil))
+print("List yang menampilkan hanya key genap: \n")
+print(shown_only_even_key(unyil), "\n\n")
+print("List yang menampilkan hanya key ganjil: \n")
+print(shown_only_odd_key(unyil), "\n\n")
+print("List yang menampilkan hanya key genap dan valuenya lebih dari 1: \n")
+print(shown_only_even_key_and_with_more_than_1_in_value(unyil), "\n\n")
+print("List yang menampilkan key ganjil dan valuenya lebih dari 1: \n")
+print(shown_only_odd_key_and_with_more_than_1_in_value(unyil))
+
+unyil = create_summary(numbers)
+# print(unyil)
+print("List yang menampilkan key yang hanya genap")
+print(shown_only_even_key(unyil))
+print('')
+print("List yang menampilkan key yang hanya ganjil")
+print(shown_only_odd_key(unyil))
+print('')
+print("List yang menampilkan key genap dan valuenya lebih dari 1")
 print(shown_only_even_key_and_with_more_than_1_in_value(unyil))
 
 
