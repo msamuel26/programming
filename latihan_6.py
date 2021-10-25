e9c5773e9b961cf27cd4230c49edbdb3628cf7c4
Moses Samuel <msamuelmulia@gmail.com>
2021-10-25 19:47:40 +0700

    Malam ini mempertajam pemahaman tentang pembuatan parameter dengan atau tanpa default value

--- latihan_6.py
+++ latihan_6.py
@@ -4,8 +4,15 @@
 def kali(param1, param2 = 2):
     return param1 * param2 
 
-def kali_dan_tambah(param1, param2, param3):
+def kali_dan_tambah(param1 = 2, param2 = 3, param3 = 4):
+    return param1 * param2 + param3
 
 print(kali(3, 5))
 print(kali(4))
 print(kali(param1=4))
+
+print(kali_dan_tambah(1, 3, 5))
+print(kali_dan_tambah(param1 = 1, param2 = 3, param3 = 5))
+print(kali_dan_tambah(param3 = 5, param1 = 1, param2 = 3))
+print(kali_dan_tambah())
+print(kali_dan_tambah(param2 = 5))
