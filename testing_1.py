b404b0f31d441680a419f32e2511218bc4192b0c
Moses Samuel <msamuelmulia@gmail.com>
2022-02-16 20:52:29 +0700

    Hari ini mempelajari cara meng-eksekusi suatu code yang menggunakan Class (tipe data baru)

--- testing_1.py
+++ testing_1.py
@@ -21,7 +21,7 @@ def testing_1(info):
         x += " "
     return x
 
-print(testing_1("abaca dabra didi"))
+# print(testing_1("abaca dabra didi"))
 
 def testing_2(data):
     data_1 = data.lower()
@@ -37,25 +37,62 @@ def testing_2(data):
 # print(testing_2("abaca dabra didi DADU"))
 
 def daftar_nilai(inputt):
-    x = {}
+    i = {}
     for data in inputt:
-        if data[0] not in x:
-            x[data[0]] = [0,0,0]
+        if data[0] not in i:
+            i[data[0]] = [0,0,0]
         if data[1] == "math":
-            x[data[0]][0] = data[2]
-        if data[1] == "english":
-            x[data[0]][1] = data[2]
-        if data[1] == "science":
-            x[data[0]][2] = data[2]
-    return x
+            i[data[0]][0] = data[2]
+        elif data[1] == "english":
+            i[data[0]][1] = data[2]
+        elif data[1] == "science":
+            i[data[0]][2] = data[2]
+    return i
 
-print(daftar_nilai([["acong", "math", 10],
-            ["badu", "english", 9],
-            ["amoy", "science", 9],
-            ["acong", "english", 8],
-            ["badu", "science", 7],
-            ["amoy", "english", 8],
-            ["acong", "science", 8],
-            ["badu", "math", 7],
-            ["amoy", "math", 9]]
-        ))
+# print(daftar_nilai([["acong", "math", 10],
+#             ["badu", "english", 9],
+#             ["amoy", "science", 9],
+#             ["acong", "english", 8],
+#             ["badu", "science", 7],
+#             ["amoy", "english", 8],
+#             ["acong", "science", 8],
+#             ["badu", "math", 7],
+#             ["amoy", "math", 9]]
+#         ))
+
+class ShowRanking:
+    def __init__(self):
+        self.input = None 
+        self.i = {}
+
+    def ranking(self, input):
+        for data in input:
+            if data[0] not in self.i:
+                self.i[data[0]] = [0,0,0,0]
+            if data[1] == "math":
+                self.i[data[0]][0] = data[2]     
+            if data[1] == "english":
+                self.i[data[0]][1] = data[2]
+            if data[1] == "science":
+                self.i[data[0]][2] = data[2]
+            self.i[data[0]][-1] += data[2]
+        # y = self.i.items()
+        # for item in y:
+        #     n = item[1][0] + item[1][1] + item[1][2]
+        #     item[1][-1] += n
+        results = sorted([[name, score1, score2, score3, total]
+            for name, (score1, score2, score3, total) in self.i.items()],
+            key=lambda z: (z[4], z[3]), reverse=True)
+        return results 
+
+a = ShowRanking()
+print(a.ranking([["acong", "math", 10],
+ ["badu", "english", 9],
+ ["amoy", "science", 9],
+ ["acong", "english", 8],
+ ["badu", "science", 7],
+ ["amoy", "english", 8],
+ ["acong", "science", 8],
+ ["badu", "math", 7],
+ ["amoy", "math", 9]]
+))
