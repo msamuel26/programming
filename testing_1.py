08d903f5320c48346eda9ee00704b7180bb5fb6a
Moses Samuel <msamuelmulia@gmail.com>
2022-02-17 20:54:54 +0700

    Mempelajari tentang cara menginstansi menjalankan suatu code yang ada Classnya

--- testing_1.py
+++ testing_1.py
@@ -70,20 +70,20 @@ class ShowRanking:
             if data[0] not in self.i:
                 self.i[data[0]] = [0,0,0,0]
             if data[1] == "math":
-                self.i[data[0]][0] = data[2]     
-            if data[1] == "english":
+                self.i[data[0]][0] = data[2]
+            elif data[1] == "english":
                 self.i[data[0]][1] = data[2]
-            if data[1] == "science":
+            elif data[1] == "science":
                 self.i[data[0]][2] = data[2]
-            self.i[data[0]][-1] += data[2]
-        # y = self.i.items()
-        # for item in y:
-        #     n = item[1][0] + item[1][1] + item[1][2]
-        #     item[1][-1] += n
+            # self.i[data[0]][-1] += data[2]
+        y = self.i.items()
+        for item in y:
+            c = item[1][0] + item[1][1] + item[1][2]
+            item[1][-1] += c
         results = sorted([[name, score1, score2, score3, total]
             for name, (score1, score2, score3, total) in self.i.items()],
-            key=lambda z: (z[4], z[3]), reverse=True)
-        return results 
+            key=lambda n: (n[4], n[3]), reverse=True)
+        return results  
 
 a = ShowRanking()
 print(a.ranking([["acong", "math", 10],
@@ -96,3 +96,49 @@ print(a.ranking([["acong", "math", 10],
  ["badu", "math", 7],
  ["amoy", "math", 9]]
 ))
+
+class Saring:
+    def __init__(self):
+        self.data = []
+
+    def saring(self, input, name):
+        for data in input:
+            if data[0] == name:
+                self.data.append(data)
+        return self.data
+
+i = Saring()
+print(i.saring([["acong", "math", 10],
+ ["badu", "english", 9],
+ ["amoy", "science", 9],
+ ["acong", "english", 8],
+ ["badu", "science", 7],
+ ["amoy", "english", 8],
+ ["acong", "science", 8],
+ ["badu", "math", 7],
+ ["amoy", "math", 9]], "acong"
+))
+
+class Saring2:
+    def __init__(self, input):
+        self.data = []
+        self.input = input 
+
+    def saring(self, name):
+        self.data = []
+        for data in self.input:
+            if data[0] == name:
+                self.data.append(data)
+        return self.data 
+
+e = Saring2([["acong", "math", 10],
+ ["badu", "english", 9],
+ ["amoy", "science", 9],
+ ["acong", "english", 8],
+ ["badu", "science", 7],
+ ["amoy", "english", 8],
+ ["acong", "science", 8],
+ ["badu", "math", 7],
+ ["amoy", "math", 9]])
+print(e.saring("acong"))
+print(e.saring("badu")) 
