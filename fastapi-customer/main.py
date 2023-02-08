c02c7d264b12ca4abc7fe67ebe8298c48708b297
Moses Samuel <msamuelmulia@gmail.com>
2023-02-09 04:56:08 +0700

    Melengkapi FastAPI mapping CRUD untuk Customer

--- fastapi-customer/main.py
+++ fastapi-customer/main.py
@@ -44,3 +44,22 @@ async def post_add_customer(request: Request, customer: CustomerCreate, db: Sess
     print(customer)
     new_customer = create_customer(db, **customer.dict())
     return {"message": "Customer created successfully"}
+
+@app.get("/customer/edit/{customer_id}", response_class=JSONResponse)
+def get_edit(request: Request, customer_id: int, db: Session = Depends(get_db)):
+    print("ini get edit")
+    customer = get_customer(db, customer_id)
+    return JSONResponse(content = {"id": customer.id, "first_name": customer.first_name, "last_name": customer.last_name, "age": customer.age, "country": customer.country})
+
+@app.put("/customer/edit/{customer_id}", response_class=JSONResponse)
+def put_edit(request: Request, customer_id: int, first_name: str, last_name: str, age: int, country: str, db: Session = Depends(get_db)):
+    print("ini put edit")
+    update_customer(db, customer_id=customer_id, first=first_name, last=last_name, age=age, country=country)
+    return {"message": "Customer created successfully"}
+
+
+@app.delete("/customer/delete/{customer_id}", response_class=JSONResponse)
+def delete(customer_id: int, db: Session = Depends(get_db)):
+    print("ini untuk delete")
+    delete_customer(db, customer_id)
+    return {"message": "Customer deleted successfully"}
