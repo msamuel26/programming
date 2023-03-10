c262ba06b3008b9a5e4a5d04b94817212f61fa5a
Bayu Cahya <bayucahyap@gmail.com>
2023-03-10 21:00:27 +0700

    program utama dan menyertakan customer routes

---
+++ fastapi-trans/main.py
@@ -0,0 +1,19 @@
+import uvicorn
+from fastapi import FastAPI
+
+from api.customer import customer_router
+from extensions.database import Base, engine
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {'message': 'Moses Transactional <3'}
+
+
+app.include_router(customer_router)
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8080)
