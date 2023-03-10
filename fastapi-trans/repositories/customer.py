148003a434587459237d49e84f036ae91fb3d80c
Bayu Cahya <bayucahyap@gmail.com>
2023-03-10 21:43:17 +0700

    remove db from parameters

--- fastapi-trans/repositories/customer.py
+++ fastapi-trans/repositories/customer.py
@@ -2,13 +2,13 @@ from decorators.database import db
 from schemas.customer import CustomerInfo
 
 @db
-def save(customer, db):
+def save(customer):
     db.add(customer)
     db.flush()
     return CustomerInfo.from_orm(customer)
 
 @db
-def delete(customer, db):
+def delete(customer):
     db.delete(customer)
     db.flush()
     return CustomerInfo.from_orm(customer)
