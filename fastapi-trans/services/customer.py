9d811c67834f0d76f9354ad2bd50c43e95d3d108
Bayu Cahya <bayucahyap@gmail.com>
2023-03-10 22:34:15 +0700

    add ()

--- fastapi-trans/services/customer.py
+++ fastapi-trans/services/customer.py
@@ -24,7 +24,7 @@ def update_customer(customer_id: int, first: str, last: str, age: int, country: 
     save(customer)
     return customer
 
-def get_customers(skip: int = 0, limit: int = 100, db=SessionMaker):
+def get_customers(skip: int = 0, limit: int = 100, db=SessionMaker()):
     return db.query(CustomerModel).offset(skip).limit(limit).all()
 
 @transactional
